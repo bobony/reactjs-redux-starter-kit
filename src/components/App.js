6f822c6e0d8cb642f9c4a9a82e2923fcb08335e0
Anil Kumar <anilkumar2308@hotmail.com>
2016-07-24 22:45:45 +0530

    Added Webpack production build config file

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React , {Component, PropTypes} from 'react';
 
 class App extends Component {
   render(){
-    return (
-  		<div>{this.props.children}</div>
-    );
+  return (
+   <div>{this.props.children}</div>
+   );
   }
 }
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,12 @@
 import React , {Component, PropTypes} from 'react';
 
 class Home extends Component {
-  render(){
-    return (
-  	  <div>
-      <h1>Reactjs, Redux, Webpack and babel are the perfect ingredients for you reciepe :) </h1>
-      </div>
-    );
+	render(){
+		return (
+			<div>
+			<h1>Reactjs, Redux, Webpack and babel are the perfect ingredients for you reciepe :) </h1>
+			</div>
+		);
   }
 }
 
